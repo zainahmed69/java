94a63cb2bb43e710453dbf8a349a93795a61bafc
zainahmed69 <zainahmedr23@gmail.com>
2021-04-04 16:33:41 +0500

    First commit

---
+++ src/com/codewirthmosh/Main.java
@@ -0,0 +1,9 @@
+package com.codewirthmosh;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name="Zain";
+        System.out.println("Hello World "+name);
+    }
+}
